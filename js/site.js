2f937b73a2091912c30f3d7dc95f0989940a1e90
brianley99 <Brianley99@gmail.com>
2022-07-12 19:36:02 -0700

    connected js to app
    added basic function structure

--- js/site.js
+++ js/site.js
@@ -1,1 +1,24 @@
+//get the values from the page
+//starts or controller function
+function getValues() {
+    //get values from page
+    let startValue = document.getElementById("startValue").value;
+    let endValue = document.getElementById("endValue").value;
 
+    alert('The Start Value: ' + startValue)
+    //call generateNumbers
+
+    //call displayNumbers
+}
+
+//generate numbers from strartValue to endValue
+//logic function
+function generateNumbers() {
+
+}
+
+//display the numbers and mark the even bold
+//display or view functions
+function displayNumbers() {
+
+}
